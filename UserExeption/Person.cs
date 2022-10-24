63c861ff074d4c1d9139850dd137f5b819c2e7af
Andrey Popov <andrej_1987@bk.ru>
2022-10-24 19:26:55 +0500

    Добавьте файлы проекта.

---
+++ UserExeption/Person.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserExeption
+{
+    internal class Person_12Plus
+    {
+        private int age;
+        public string Name { get; set; } = "User" + DateTime.Now.ToString("yyyy.MM.dd-HH:mm:ss.fff");
+        public int Age
+        {
+            get { return age; } // get => age;
+            set
+            {
+                if (value < 0) // throw new Exception("Некорректный возраст");
+                {
+                    throw new Exception("Некорректный возраст");
+                }
+                else
+                {
+                    if (value < 12)
+                    {
+                        throw new Exception("Возрастное ограничение 12+");
+                    }
+                    else age = value;
+                }
+            }
+        }
+    }
+    internal class RetiredPeople
+    {
+        private int age;
+        private bool sex;
+        public string Name { get; set; } = "User" + DateTime.Now.ToString("yyyy.MM.dd-HH:mm:ss.fff");
+        public int Age
+        {
+            get { return age; } 
+            set
+            {
+                if (value < 0 && sex == true) 
+                {
+                    throw new Exception("Некорректный возраст");
+                }
+                else
+                {
+                    if (value < 65 && sex == true)
+                    {
+                        throw new Exception("Пенсионный возраст мужчины 65 лет.");
+                    }
+                    else age = value;
+                }
+                if (value < 0 && sex == false)
+                {
+                    throw new Exception("Некорректный возраст");
+                }
+                else
+                {
+                    if (value < 60 && sex == false)
+                    {
+                        throw new Exception("Пенсионный возраст женщины 60 лет.");
+                    }
+                    else age = value;
+                }
+            }
+               
+        }
+        public bool Sex
+        {
+            get { return sex; }
+            set
+            {
+                if (value == true) 
+                {
+                    sex = true;
+                }
+                else
+                {
+                    sex = false;
+                }
+            }
+        }
+    }
+}
---
+++ UserExeption/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserExeption
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var RP = new RetiredPeople();
+            Console.WriteLine($"Создан пользователь {RP.Name} введите его имя:");
+            
+            RP.Name = Console.ReadLine();
+            
+            try
+            {
+                Console.WriteLine($"Задано имя пользователя {RP.Name} введите пол (true - мужской, false - женский):");
+                RP.Sex = Convert.ToBoolean(Console.ReadLine());
+                Console.WriteLine($"Введите возраст {RP.Name}: ") ;
+                RP.Age = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e002)
+            {
+                Console.WriteLine("Пользовательские ошибки {0}", e002.Message);
+            }
+
+        }
+    }
+}
